8ceb44b1b2f85a29845e4a13b94ef74ea26b1a0d
Anthony Matheus <antyoma25@gmail.com>
2025-08-28 18:47:53 +0200

    19. Terraform Input Variables Block

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
-    "Terraform" = "true"
+    "Name" = "Web EC2 Server"
   }
 }
 
@@ -156,4 +156,16 @@ resource "aws_security_group" "my-new-security-group" {
 
 resource "random_id" "randomness" {
   byte_length = 16
+}
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
 }
