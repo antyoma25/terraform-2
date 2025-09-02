65e254cbdab20aa135d185c39ee225c0bf086cb7
Anthony Matheus <antyoma25@gmail.com>
2025-09-02 18:03:18 +0200

    21. Terraform Data Block

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_vpc" "vpc" {
     Name        = var.vpc_name
     Environment = "demo_environment"
     Terraform   = "true"
+    region      = data.aws_region.current.description
   }
 }
 
@@ -116,8 +117,25 @@ resource "aws_internet_gateway" "internet_gateway" {
 #   }
 # }
 
+# Terraform Data Block - Lookup Ubuntu 22.04
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-00bbc84273381"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
