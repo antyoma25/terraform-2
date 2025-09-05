6058d5e7e2690e86caf7309cd79a360072020b81
Anthony Matheus <antyoma25@gmail.com>
2025-09-05 17:41:31 +0200

    27. Using Multiple Terraform Providers

--- providers/terraform.tf
+++ providers/terraform.tf
@@ -7,5 +7,17 @@ terraform {
     source = "hashicorp/aws"
     version = "~> 6.0.0"
    }
- }
+    http = {
+      source = "hashicorp/http"
+      version = "3.5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.3"
+    }
+  }
 }
